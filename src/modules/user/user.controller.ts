4d562b6fd7185896df21ec6f76d182cd78ab90a6
OlgaSavina <savina5565@example.com>
2023-11-13 11:30:53 +0200

    fix: I've removed unused variables according to eslint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
 
 import { User } from 'src/common/entities/user.entity';
 
