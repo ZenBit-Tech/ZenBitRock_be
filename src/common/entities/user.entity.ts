fbab141e355cb3673bcb9fb974a0c3f838f25d19
canterbery <wotphantommail@gmail.com>
2023-11-16 19:09:39 +0200

    feat: + verification fields

--- src/common/entities/user.entity.ts
+++ src/common/entities/user.entity.ts
@@ -1,7 +1,8 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CoreEntity } from './core.entity';
 
 @Entity()
-export class User {
+export class User extends CoreEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
@@ -10,4 +11,18 @@ export class User {
 
   @Column()
   password: string;
+
+  @Column({
+    type: 'boolean',
+    name: 'is_verified',
+    default: false,
+  })
+  isVerified: boolean;
+
+  @Column({
+    type: 'varchar',
+    name: 'veritication_code',
+    default: '',
+  })
+  verificationCode: string;
 }
