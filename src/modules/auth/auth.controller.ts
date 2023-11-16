124f51a32865c46b65fd501df4b01636a77610cb
canterbery <wotphantommail@gmail.com>
2023-11-16 19:12:22 +0200

    feat: + confirm email endpoint and handler

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { User } from 'src/common/entities/user.entity';
@@ -9,7 +9,8 @@ import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-
+import { ConfirmEmailDto } from './dto/confirm-email.dto';
+import { UpdateResult } from 'typeorm';
 
 @Controller('auth')
 @ApiBearerAuth()
@@ -31,5 +32,14 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   async getProfile(@Request() req): Promise<User> {
     return req.user;
+  }
+
+  @Post('confirm-email')
+  @ApiOperation({ summary: 'Confirm email address' })
+  @ApiResponse({ status: 200, description: 'Email address successfully confirmed' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async confirmEmail(@Body() confirmEmailDto: ConfirmEmailDto): Promise<UpdateResult> {
+    const { code, email } = confirmEmailDto;
+    return this.authService.confirmEmail(email, code);
   }
 }
