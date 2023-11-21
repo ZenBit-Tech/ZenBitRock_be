4240315b789988f80cf6c7a203b9028a62336b29
OlgaSavina <savina5565@example.com>
2023-11-21 11:35:54 +0200

    fix: add types

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -20,7 +20,7 @@ export class EmailService {
     private mailerService: MailerService,
   ) {}
 
-  async sendEmailVerificationCode(email: string): Promise<unknown> {
+  async sendEmailVerificationCode(email: string): Promise<void> {
     const user = await this.userService.findByEmail(email);
 
     if (!user) throw new UnauthorizedException(`User doesn't exist`);
