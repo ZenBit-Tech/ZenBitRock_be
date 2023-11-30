c32cedaf680072df9557954296a043ac8ef94564
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-30 16:19:41 +0100

    fix: bug in core entity

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -7,6 +7,6 @@ export abstract class CoreEntity extends BaseEntity {
   @CreateDateColumn({ type: 'datetime', name: 'created_at' })
   public createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', default: () => 'CURRENT_TIMESTAMP(6)' })
   public updatedAt: Date;
 }
