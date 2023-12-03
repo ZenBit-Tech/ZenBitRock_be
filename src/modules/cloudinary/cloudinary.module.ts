207b2aee78da864c81583989a437836a80dc1969
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-12-03 13:52:45 +0100

    fix: deleted cloudinary controller

--- src/modules/cloudinary/cloudinary.module.ts
+++ src/modules/cloudinary/cloudinary.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from 'common/configs/config.module';
 
-import { CloudinaryController } from './cloudinary.controller';
 import { CloudinaryService } from './cloudinary.service';
 
 @Module({
   imports: [ConfigModule],
   providers: [CloudinaryService],
-  controllers: [CloudinaryController],
 })
 
 export class CloudinaryModule { }
