72d6a3914028ce4f2425c618a56eeb58b4d3c5df
canterbery <wotphantommail@gmail.com>
2023-12-14 18:54:44 +0200

    feat: + lead types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -2,3 +2,4 @@ export { UserAuthResponse } from './user-auth-response.type';
 export { UserProfileResponse } from './user-profile-response.type';
 export { UserInfoResponse } from './user-info-response';
 export { type UserSignInResponse } from './user-sign-in-response';
+export { type Lead } from './lead';
