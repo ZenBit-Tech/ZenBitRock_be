efeb9c96d73898de069495c00ebf8dff7674ec11
canterbery <wotphantommail@gmail.com>
2023-12-15 15:26:32 +0200

    feat: + getMatchingProperties

--- src/modules/lead/lead.service.ts
+++ src/modules/lead/lead.service.ts
@@ -2,7 +2,12 @@ import { lastValueFrom } from 'rxjs';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { QobrixApiPath } from 'src/common/enums';
-import { LeadDetailsResponse } from 'src/common/types/lead';
+import { getPropertySearchFilter } from './lib/helpers';
+import {
+  Lead,
+  LeadDetailsResponse,
+  MatchingPropertiesResponse,
+} from 'src/common/types';
 
 @Injectable()
 export class LeadService {
@@ -16,6 +21,34 @@ export class LeadService {
           {
             params: {
               ['include[]']: 'ConversionStatusWorkflowStages',
+            },
+          },
+        ),
+      );
+
+      if (!data) {
+        throw new NotFoundException('Not found');
+      }
+
+      const lead = data.data;
+      const matchingProperties = await this.getMatchingProperties(lead);
+
+      return { data: lead, matchingProperties: matchingProperties.data };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getMatchingProperties(lead: Lead): Promise<MatchingPropertiesResponse> {
+    const filter = getPropertySearchFilter(lead);
+
+    try {
+      const { data } = await lastValueFrom(
+        this.httpService.get<MatchingPropertiesResponse>(
+          `${process.env.QOBRIX_BASE_URL}${QobrixApiPath.PROPERTIES}`,
+          {
+            params: {
+              search: filter,
             },
           },
         ),
