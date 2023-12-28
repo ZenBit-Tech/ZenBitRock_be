9f4f9622ef96af06b67865fbfb5e71c123815a2c
OlgaSavina <savina5565@example.com>
2023-12-28 12:48:03 +0200

    feat: add chat sockets

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MessageModule } from '../message/message.module';
 
 @Module({
   imports: [
+    MessageModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
