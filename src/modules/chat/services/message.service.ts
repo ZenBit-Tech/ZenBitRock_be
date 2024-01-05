2383d87fa4d4e331fa987757a7f386864afb03bc
canterbery <wotphantommail@gmail.com>
2024-01-05 18:49:22 +0200

    feat: * params , * return type

--- src/modules/chat/services/message.service.ts
+++ src/modules/chat/services/message.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Message } from 'src/common/entities/message.entity';
-import { GetMessagesDto } from '../dto/get-messages.dto';
 import { CreateMessageDto } from '../dto/create-message.dto';
 
 @Injectable()
@@ -12,10 +11,10 @@ export class MessageService {
     private readonly messageRepository: Repository<Message>,
   ) {}
 
-  async getMessages(getMessagesDto: GetMessagesDto): Promise<Message[]> {
+  async getMessages(chatId: string): Promise<Message[]> {
     try {
       const messages = await this.messageRepository.find({
-        where: { chat: { id: getMessagesDto.chatId } },
+        where: { chat: { id: chatId } },
         relations: ['owner'],
       });
 
@@ -35,10 +34,12 @@ export class MessageService {
         chat: { id: createMessageDto.chatId },
         owner: { id: userId },
       });
+      const newMessage = await this.messageRepository.save(message);
 
-      await this.messageRepository.save(message);
-
-      return message;
+      return await this.messageRepository.findOne({
+        where: { id: newMessage.id },
+        relations: ['owner'],
+      });
     } catch (error) {
       throw new Error('Failed to create message');
     }
