a897d1691bb9f2f0a8b28be2146f8699eae6a4d9
canterbery <wotphantommail@gmail.com>
2024-01-18 22:15:45 +0200

    feat: + retries

--- src/middleware/database-ping.middleware.ts
+++ src/middleware/database-ping.middleware.ts
@@ -2,6 +2,9 @@ import { Inject, Injectable, NestMiddleware, forwardRef } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UserService } from 'src/modules/user/user.service';
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 200;
+
 @Injectable()
 export class DatabasePingMiddleware implements NestMiddleware {
   constructor(
@@ -9,10 +12,18 @@ export class DatabasePingMiddleware implements NestMiddleware {
     private UserService: UserService,
   ) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    try {
-      await this.UserService.findOne('ping');
-    } catch (error) {
-      console.error(error);
+    let attemptCount = 0;
+    let isRetrySuccessful = false;
+
+    while (attemptCount < MAX_RETRIES && !isRetrySuccessful) {
+      try {
+        await this.UserService.findOne('ping');
+        isRetrySuccessful = true;
+      } catch (error) {
+        console.error(error);
+        attemptCount++;
+        await new Promise((res) => setTimeout(res, RETRY_DELAY));
+      }
     }
 
     next();
