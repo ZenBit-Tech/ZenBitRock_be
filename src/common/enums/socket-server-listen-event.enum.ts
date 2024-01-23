8e7f4c7b71b0388f2faa15a0430fd9f1ce40162e
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2024-01-23 10:24:34 +0100

    feat: added events

--- src/common/enums/socket-server-listen-event.enum.ts
+++ src/common/enums/socket-server-listen-event.enum.ts
@@ -3,6 +3,8 @@ const ChatEvent = {
   NewMessage: 'new_message',
   RequestAllChats: 'request_all_chats',
   NewChat: 'new_chat',
+  ChatDeleted: 'chat_deleted',
+  ChatUpdated: 'chat_updated',
 } as const;
 
 export { ChatEvent };
