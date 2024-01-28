94875d1f2574c32fa0c702b38dde1810a86a88fc
canterbery <wotphantommail@gmail.com>
2024-01-28 11:32:39 +0200

    feat: + notification module

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -4,11 +4,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChatModule } from '../chat/chat.module';
 import { UserModule } from '../user/user.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
   imports: [
     ChatModule,
     UserModule,
+    NotificationModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
